67c0e8642d8bc390e2cf313f2ce59c344c339ca8
taufiqgiri <taufiq.ichwanusofa@ist.id>
2023-01-07 09:37:12 +0700

    feat: add new component calculator keys

--- src/pages/CalculatorPage.js
+++ src/pages/CalculatorPage.js
@@ -1,24 +1,32 @@
 import React from "react";
 import './index.css';
+import keysData from '../data/data-keys.json'
 
 export default function CalculatorPage() {
   return (
     <div className="pt-5 flex flex-col justify-center items-center">
       <div className="p-5 border border-black mt-5 flex flex-col justify-center items-center rounded">
-        <div className="w-200 flex flex-row justify-start items-start">
+        <div className="w-300 flex flex-row justify-start items-start">
           <p className="font-sans text-2xl font-bold">
             Web Calculator
           </p>
         </div>
-        <div className="p-5 border border-black rounded bg-gray-200 w-200 h-50">
+        <div className="p-5 border border-black rounded bg-gray-200 w-300 h-75">
           
         </div>
-        <div className="w-200 flex flex-row flex-wrap mt-3">
+        <div className="w-300 flex flex-row flex-wrap mt-3">
           {
-            ["1", "2", "3", "4", "5", "6"].map((data) => {
+            keysData.map((data) => {
               return (
-                <div className="s-100 flex justify-center items-center border">
-                  {data}
+                <div 
+                  className={
+                    `${data.box === 'single' ? 's-75' : 's-150'} 
+                    flex justify-center items-center border
+                    ${data.backgroundColor === 'black' ? 'bg-gray-700' : data.backgroundColor === 'gray' ? 'bg-gray-400' : 'bg-yellow-500'}
+                    text-white text-2xl key-pad`
+                  }
+                >
+                  {data.symbol}
                 </div>
               )
             })
